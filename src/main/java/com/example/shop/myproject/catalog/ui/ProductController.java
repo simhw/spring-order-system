a651e1446c23f7d1b7ea824fc33f571bc081cb87
simhw <simhw96@gmail.com>
2024-08-26 21:46:11 +0900

    feat: 카테고리 조회

--- src/main/java/com/example/shop/myproject/catalog/ui/ProductController.java
+++ src/main/java/com/example/shop/myproject/catalog/ui/ProductController.java
@@ -1,21 +1,13 @@
 package com.example.shop.myproject.catalog.ui;
 
-import com.example.shop.myproject.catalog.command.domain.product.ProductRepository;
 import com.example.shop.myproject.catalog.query.category.CategoryData;
-import com.example.shop.myproject.catalog.query.product.CategoryProduct;
 import com.example.shop.myproject.catalog.query.product.ProductQueryService;
-import com.example.shop.myproject.common.ApiResponse;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
--- src/main/java/com/example/shop/myproject/catalog/ui/ProductRestController.java
+++ src/main/java/com/example/shop/myproject/catalog/ui/ProductRestController.java
@@ -14,9 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @AllArgsConstructor
